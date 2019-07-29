6a957af45b97ab29087692450b61a3a02212b64f
Henry Holmes <henry@syntactile.com>
2019-07-29 17:15:22 -0400

    Project page responsiveness, data

--- pages/project/_project.vue
+++ pages/project/_project.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <div class="projectCover flex block items-end px-4 md:px-0 pt-16 bg-cover bg-center bg-scroll md:bg-fixed text-white scrim-b"
+    <div class="projectCover flex block items-end px-4 md:px-0 pt-16 bg-cover bg-bottom bg-scroll md:bg-fixed text-white scrim-b"
               :style="{ backgroundImage: `url('${ project.cover }')` }">
-      <div class="w-full max-w-3xl mx-auto text-xl z-10 mb-4">
+      <div class="w-full max-w-3xl xl:max-w-4xl mx-auto text-xl z-10 mb-4">
         <h1 class="mb-2 md:mb-4 text-3xl md:text-6xl">{{ project.title }}</h1>
         <p class="leading-snug mb-4 md:mb-8">{{ project.subtitle }}</p>
       </div>
     </div>
-    <section class="flex flex-wrap max-w-3xl mx-auto">
+    <section class="flex flex-wrap max-w-3xl xl:max-w-4xl mx-auto">
       <PlainList
         class="projectContext my-8 md:my-0"
         :header="'Timeframe'" 
-        :items="[project.start]"
+        :items="[timeframeText]"
         />
       <PlainList
         class="projectContext my-8 md:my-0"
@@ -29,14 +29,14 @@
         :items="project.tools"
         />
     </section>
-    <section class="py-16 max-w-3xl mx-auto">
+    <section class="py-16 max-w-3xl xl:max-w-4xl mx-auto">
       <div 
         v-for="section in project.sections" 
         :key="section.id"
         class="flex flex-wrap mb-16">
         <h2 class="w-full md:w-1/4">{{ section.heading }}</h2>
         <div 
-          class="w-full md:w-3/4"
+          class="w-full md:w-1/2"
           v-html="$md.render(section.body)" />
         <!-- <span class="inline-block bg-gray-lighter rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
           {{ section.topic }}
@@ -64,15 +64,11 @@ export default {
       title: this.project.title
     }
   },
-  // computed: {
-  //   rolesWithKeys: function () {
-  //     rolesArray = [];
-  //     this.project.roles.forEach(function (item, id) {
-  //       rolesArray.push({'title',item})
-  //     });
-  //     return rolesArray;
-  //   }
-  // },
+  computed: {
+    timeframeText() {
+      return this.project.start + ' → ' + this.project.end; 
+    }
+  },
   components: {
     PlainList
   }
